90798d5c78097daab44ca0b900dfb978e4e20abd
João Alexandre <aleramosfilho@gmail.com>
2025-03-18 03:55:25 -0300

    set up storage service

---
+++ src/database/storage.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+
+pub const Storage = struct {
+    file: std.fs.File,
+    pub fn init(path: []const u8) !Storage {
+        const file = try std.fs.cwd().createFile(path, .{ .read = true, .write = true, .append = true });
+        return Storage{ .file = file };
+    }
+
+    pub fn append(self: *Storage, command: []const u8) !void {
+        try self.file.writer().writeAll(command);
+        try self.file.writer().writeAll("\n");
+        try self.file.sync();
+    }
+    pub fn close(self: *Storage) void {
+        self.close();
+    }
+};
